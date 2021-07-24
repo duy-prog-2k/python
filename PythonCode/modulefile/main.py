934173ad43dabfaadd62b53bd5646b99a4435fe8
duy-prog-2k <phamkhacduy.it@gmail.com>
2021-07-24 10:51:20 +0700

    add code module

---
+++ PythonCode/modulefile/main.py
@@ -0,0 +1,6 @@
+import mymodule as mm
+
+mm.greeting()
+
+name = mm.person['name']
+print(name)
---
+++ PythonCode/modulefile/mymodule.py
@@ -0,0 +1,8 @@
+def greeting():
+  print('Hello, from mymodule.py')
+pass
+
+person = {
+  'name': 'Pham Khac Duy',
+  'age': 21, 
+}
