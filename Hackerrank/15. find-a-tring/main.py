927f0c7026e87f3eb463918c659c61125d46463a
duy-prog-2k <phamkhacduy.it@gmail.com>
2021-09-28 23:20:28 +0700

    add comment 15

--- Hackerrank/15. find-a-tring/main.py
+++ Hackerrank/15. find-a-tring/main.py
@@ -1,4 +1,6 @@
 # https://www.hackerrank.com/challenges/find-a-string/problem
+# Ý tưởng của tui là tui sẽ cắt cái list bằng với substring đối với mỗi từ trong list
+# Sau só tui sẽ so sánh những cái list đã được slide đó với substring, nếu bằng thì count + 1
 a = 'ABCDCDC'
 count = 0
 for i in range(0, len(a)):
